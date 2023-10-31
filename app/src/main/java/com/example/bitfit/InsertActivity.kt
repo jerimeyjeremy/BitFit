1a5f784410b63795fb884e4024c7e290b59c3a9f
jerimeyjeremy <69161534+jerimeyjeremy@users.noreply.github.com>
2023-10-31 18:56:08 -0500

    Initial commit

---
+++ app/src/main/java/com/example/bitfit/InsertActivity.kt
@@ -0,0 +1,83 @@
+package com.example.bitfit
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.launch
+
+class InsertActivity : AppCompatActivity() {
+    private lateinit var foodEditText: EditText
+    private lateinit var caloriesEditText: EditText
+    private lateinit var foodTextView: TextView
+    private lateinit var caloriesTextView: TextView
+    private lateinit var recordButton: Button
+
+    var foods : MutableList<DisplayFit> = ArrayList()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_insert)
+
+        recordButton = findViewById(R.id.recordBtn)
+        foodEditText = findViewById(R.id.foodET)
+        caloriesEditText = findViewById(R.id.caloriesET)
+        foodTextView = findViewById(R.id.foodTV)
+        caloriesTextView = findViewById(R.id.caloriesTV)
+
+        recordButton.setOnClickListener {
+            val foodName = foodEditText.text
+            val foodCalories = caloriesEditText.text
+
+            lifecycleScope.launch(IO) {
+                (application as FoodApplication).db.foodDao().insert(
+                    FitEntity(
+                        name = foodName.toString(),
+                        calories = foodCalories.toString().toDouble()
+                    )
+                )
+            }
+            finish()
+        }
+
+
+       // val food = intent.getSerializableExtra(ARTICLE_EXTRA) as DisplayFit
+
+      //  foodTextView.text = food.food
+       // caloriesTextView.text = food.calories.toString()
+
+
+       // foodList : MutableList<DisplayFit> = ArrayList()
+
+       // fun addFood(food: String, calories: Editable?) {
+        //  foodList.add(DisplayFit(food, calories))
+       // }
+
+       // fun getFood(): MutableList<DisplayFit> {
+       //     return foodList
+       // }
+
+//        val foodsRV = findViewById<RecyclerView>(R.id.articles) as RecyclerView
+
+//        foods = getFood()
+//        val adapter = FitAdapter(foods)
+//        foodsRV.adapter = adapter
+//        foodsRV.layoutManager = LinearLayoutManager(this)
+
+       // addFood.setOnClickListener {
+       //     var foodName : String = foodEdit.text.toString()
+        //    var caloriesName : Editable? = caloriesEdit.text
+
+       //     var newFood = addFood(foodName,caloriesName)
+        //    foods = getFood()
+ //           adapter.notifyDataSetChanged()
+
+         //   val intent = Intent(this, MainActivity::class.java).apply{}
+         //   startActivity(intent)
+       // }
+    }
+}
